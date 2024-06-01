76afacd04c9d0028bbb795ba5ee5539241ded3a4
Dheeraj Verma <164219473+DheerajVerma2017@users.noreply.github.com>
2024-06-01 16:43:05 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,31 @@
+class Demo
+{
+   // 1 public static void main(String args[])
+  //  2 public static void main(String s[])  // String types ka Array Hai
+  public static void main(String...s) //Java Me JDK 1.5 Alternative
+    //static public void main(String...s) //Java Me JDK 1.5 Alternative
+  {
+   System.out.println("Welcome Java Hello World");
+  }
+ }
+ /*
+ Practice java
+  Point 1 Direct Run .java con be do
+  java Demo.java
+ */
+ 
+ /*
+  point2
+  jshell
+|  Welcome to JShell -- Version 14.0.2
+|  For an introduction type: /help intro
+
+ jshell> System.out.println("Hello Welcome Java");
+ Hello Welcome Java
+
+jshell> 
+
+*/
+
+  
+  
